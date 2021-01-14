8e650d5a9fb0daea78615c28fa739a18fcd46823
Pooja Rao <66176876+myGitRao@users.noreply.github.com>
2021-01-15 00:32:23 +0530

    Add files via upload

---
+++ 08_StringSlicing.py
@@ -0,0 +1,9 @@
+mystr = "sky is Pink"
+print(len(mystr))
+print(mystr[::-2])
+
+print(mystr.endswith("bdoy"))
+print(mystr.count("P"))
+print(mystr.capitalize())  # Only converts first letter to capital
+print(mystr.upper())
+print(mystr.replace("is", "are"))
