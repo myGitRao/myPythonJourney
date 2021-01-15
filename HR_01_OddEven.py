9521af4a09c68cbcf73410563f7cdac5d15d9dcd
Pooja Rao <66176876+myGitRao@users.noreply.github.com>
2021-01-15 23:28:37 +0530

    Add files via upload

---
+++ HR_01_OddEven.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__' :
+    n = int(input("Number: "))
+if n % 2 != 0:
+    print("Not Weird ")
+elif (n % 2 == 0) and n in range(2, 6):
+    print("Not Weird")
+elif (n % 2 == 0) and n in range(6, 21):
+    print("Weird")
+else:
+    print("Not Weird")
+
+
+
---
+++ HR_03_ArithematicOperators.py
@@ -0,0 +1,20 @@
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+#
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Output
+# 3
+# -2
+# 1
+
+if __name__ == '__main__':
+    a = int(input("a: "))
+    b = int(input("b: "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+
+
