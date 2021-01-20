10191b11ac645aee5878dd2f7c5b61ebf35ce683
Pooja Rao <66176876+myGitRao@users.noreply.github.com>
2021-01-20 23:23:09 +0530

    Add files via upload

---
+++ QuickQuiz_03.py
@@ -0,0 +1,12 @@
+# if-else driving example
+
+print("What is your age?")
+age = int(input())
+if age<18:
+    print("You cannot drive")
+
+elif age==18:
+    print("We will think about you")
+
+else:
+    print("You can drive")
